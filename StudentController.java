52c06a05b49f13959fc0faa85cd9c86a09fe8564
BhargaviGanta <bhargavigantaa@gmail.com>
2024-10-15 11:00:55 +0530

    Add files via upload

--- StudentController.java
+++ StudentController.java
@@ -22,4 +22,23 @@ public class StudentController {
 	        Student savedStudent = studentService.saveStudent(student);
 	        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
 	    }
+	    
+	    @GetMapping("/{id}")
+	    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+	        Optional<Student> student = studentRepository.findById(id);
+	        return student.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	    }
+	        
+	        @PutMapping("/{id}")
+	        public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student studentDetails) {
+	            Optional<Student> optionalStudent = studentRepository.findById(id);
+	            if (!optionalStudent.isPresent()) {
+	                return ResponseEntity.notFound().build();
+	            }
+	            Student student = optionalStudent.get();
+	            student.setName(studentDetails.getName());
+	            student.setAge(studentDetails.getAge());
+	            studentRepository.save(student);
+	            return ResponseEntity.ok(student);
+	        }
 }
