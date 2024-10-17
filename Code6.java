2841dd9a4d710f739fd48a5c016c0baafe1aee27
BhargaviGanta <bhargavigantaa@gmail.com>
2024-10-17 10:23:06 +0530

    Add files via upload

---
+++ Code6.java
@@ -0,0 +1,41 @@
+public class Pair<T, U> {
+    private T first;
+    private U second;
+
+      public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+
+    public T getFirst() {
+        return first;
+    }
+
+        public void setFirst(T first) {
+        this.first = first;
+    }
+
+   
+    public U getSecond() {
+        return second;
+    }
+
+        public void setSecond(U second) {
+        this.second = second;
+    }
+
+    public String toString() {
+        return "Pair [first=" + first + ", second=" + second + "]";
+    }
+
+    public static void main(String[] args) {
+        Pair<Integer, String> pair1 = new Pair<>(1, "One");
+        System.out.println("Initial Pair: " + pair1);
+        System.out.println("First: " + pair1.getFirst());
+        System.out.println("Second: " + pair1.getSecond());
+        pair1.setFirst(2);
+        pair1.setSecond("Two");
+        System.out.println("Updated Pair: " + pair1);
+    }
+}
